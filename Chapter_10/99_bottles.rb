a5ce701789a4ef8a67e3d4b1e34680d99d0fa924
Sam Johnson <samuel.c.johnson2@gmail.com>
2017-11-06 12:50:16 +0000

    Chap10 Final Exercises

---
+++ Chapter_10/99_bottles.rb
@@ -0,0 +1,96 @@
+def english_number number
+    if number < 0
+        return 'Please enter a non-negative number.'
+    end
+    if number == 0
+        return 'zero'
+    end
+    
+    num_string = ''
+    
+    ones_place = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    teenagers =  ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    
+    left = number
+    write = left/1000000
+    left = left - write*1000000
+    
+    if write > 0
+        millions = english_number write
+        num_string = num_string + millions + ' million'
+        
+        if left > 0
+            num_string = num_string + ' '   # replace space with 'and' for wedding number exercise
+        end
+    end
+    
+    
+    write = left/1000
+    left = left - write*1000
+    
+    if write > 0
+        thousands = english_number write 
+        num_string = num_string + thousands + ' thousand'
+        
+        if left > 0
+            num_string = num_string + ' '
+        end
+    end
+    
+    
+    write = left/100
+    left = left - write*100
+    
+    if write > 0
+        hundreds = english_number write
+        num_string = num_string + hundreds + ' hundred'
+        if left > 0
+            num_string = num_string + ' '
+        end
+    end
+    
+    write = left/10
+    left = left - write*10
+    
+    if write > 0
+        if ((write == 1) and (left > 0))
+            num_string = num_string + teenagers[left-1]
+            
+            left = 0
+        else
+            num_string = num_string + tens_place[write-1]
+        end
+        
+        if left > 0
+            num_string = num_string + '-'
+        end
+    end
+    
+    write = left
+    left = 0
+    
+    if write > 0
+         num_string = num_string + ones_place[write-1]
+    end
+    
+    num_string
+    
+end
+
+
+initial_bottles = 99
+new_bottles = initial_bottles
+
+while new_bottles > 2
+puts "#{english_number(new_bottles)} bottles of beer on the wall, #{english_number(new_bottles)} bottles of beer!" 
+
+new_bottles -= 1
+
+puts "You take one down and pass it around, #{english_number(new_bottles)} bottles of beer on the wall"
+
+initial_bottles = new_bottles
+end
+
+puts "Two bottles of beer on the wall, two bottles of beer! You take one down, pass it around, one bottle of beer left on the wall!"
+puts "One bottle of beer on the wall, one bottles of beer! You take one down, pass it around, no bottles of beer left on the wall!"
